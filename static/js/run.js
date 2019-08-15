b2672c8399e3e64e2c4d5cf5263a02968439bb74
JackSnowdon <jacksnowdondrums@gmail.com>
2019-08-15 15:32:58 +0000

    1-6 random number made

--- static/js/run.js
+++ static/js/run.js
@@ -1,5 +1,12 @@
+function die6() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
 $(document).ready(function(){
-  $(".test").click(function(){
-    $(this).css("color", "green")
+  
+  $("#roll").click(function(){
+    $('#die').text(die6);
   });
+  
+  
 });
