757f16eeff45455b3b15bb971a8b5f0cd039297a
JackSnowdon <jacksnowdondrums@gmail.com>
2019-08-15 15:48:47 +0000

    reformated die6 roll so it is saved into a var

--- static/js/run.js
+++ static/js/run.js
@@ -1,12 +1,19 @@
-function die6() {
-  return Math.floor(Math.random() * 6) + 1;
-}
+$(document).ready(function() {
 
-$(document).ready(function(){
-  
-  $("#roll").click(function(){
-    $('#die').text(die6);
+  $("#roll").click(function() {
+    rollD6();
   });
   
-  
+  function rollD6() {
+    var result = getD6Roll();
+    $("#die").text(result);
+    console.log(result);
+  }
+
+
+  function getD6Roll() {
+    var d6Roll = Math.floor(Math.random() * 6) + 1;
+    return d6Roll;
+  }
+
 });
